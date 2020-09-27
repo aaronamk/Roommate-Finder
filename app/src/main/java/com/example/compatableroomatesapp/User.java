a4550b720601c99d907b3bbc7094a66805da78b6
aaronamk <amklein@email.wm.edu>
2020-09-27 18:45:34 -0400

    Added bio.

--- app/src/main/java/com/example/compatableroomatesapp/User.java
+++ app/src/main/java/com/example/compatableroomatesapp/User.java
@@ -2,14 +2,16 @@ package com.example.compatableroomatesapp;
 
 public class User {
 
-    public String fullName, email;
+    public String fullName, email, bio;
 
-    public User(){
+    public User() {
 
     }
 
-    public User(String fullName, String email){
+    public User(String fullName, String email) {
         this.fullName = fullName;
         this.email = email;
     }
+
+    public void setBio(String bio) { this.bio = bio; }
 }
