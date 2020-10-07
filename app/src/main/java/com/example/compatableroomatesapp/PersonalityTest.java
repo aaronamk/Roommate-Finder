9f268f9129958d078ed3732dc6dec1b886c08242
JessicaPessoWM <jlpesso@email.wm.edu>
2020-10-07 02:31:00 -0400

    I added functionality to the "next" button and can use the editText portion for inputting their personality type.  if the user doesn't add a personality type they will get an error

--- app/src/main/java/com/example/compatableroomatesapp/PersonalityTest.java
+++ app/src/main/java/com/example/compatableroomatesapp/PersonalityTest.java
@@ -3,12 +3,17 @@ package com.example.compatableroomatesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class PersonalityTest extends AppCompatActivity {
+public class PersonalityTest extends AppCompatActivity implements View.OnClickListener{
 
     private WebView webView;
+    private EditText editPersonalityType;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,32 @@ public class PersonalityTest extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://openpsychometrics.org/tests/OEJTS/1.php");
 
+        editPersonalityType = (EditText) findViewById(R.id.editPersonalityType);
+
+        nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.nextButton:
+                updatePersonality();
+                break;
+        }
+    }
+
+    private void updatePersonality()
+    {
+        final String personalityType = editPersonalityType.getText().toString().trim();
+
+        if(personalityType.isEmpty())
+        {
+            editPersonalityType.setError("Personality Type is required!");
+            editPersonalityType.requestFocus();
+        }
     }
 }
